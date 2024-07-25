7a6b4efb508b1e122a48e35fe5e4df264b9e672b
Gordon Crone <g.crone@ucl.ac.uk>
2024-07-25 16:54:46 +0100

    Enhanced listing of apps/segments in listApps

--- apps/list_apps.cxx
+++ apps/list_apps.cxx
@@ -10,37 +10,61 @@
 #include "confmodel/Session.hpp"
 
 #include <iostream>
+//#include <set>
 #include <string>
 
 using namespace dunedaq;
 
 
 void process_segment(const confmodel::Session* session,
-                     const confmodel::Segment* segment) {
-  bool disabled = segment->disabled(*session);
-  for (auto subseg : segment->get_segments()) {
-    process_segment (session, subseg);
+                     const confmodel::Segment* segment,
+                     const std::set<std::string>& disabled_objects,
+                     std::string spacer) {
+  std::cout << spacer << "Segment " << segment->UID();
+  bool segment_disabled = segment->disabled(*session);
+  std::string reason = "";
+  if (segment_disabled) {
+    std::cout << " disabled";
+    reason = "segment";
   }
+  std::cout << "\n";
+  for (auto subseg : segment->get_segments()) {
+    process_segment (session, subseg, disabled_objects, spacer+"  ");
+  }
+
   for (auto app : segment->get_applications()) {
-    std::cout << "Application: " << app->UID();
-    if (disabled) {
-      std::cout << "<disabled segment>";
-    }
-    else {
-      auto res = app->cast<confmodel::ResourceSet>();
-      if (res) {
-        if (res->disabled(*session)) {
-          std::cout << "<disabled>";
-        }
-        else {
-          for (auto mod : res->get_contains()) {
-            std::cout << " " << mod->UID();
-            if (mod->disabled(*session)) {
-              std::cout << "<disabled>";
-            }
+    bool disabled = segment_disabled;
+    std::cout << spacer << "  Application: " << app->UID();
+    if (!disabled) {
+      auto rset = app->cast<confmodel::ResourceSet>();
+      if (rset) {
+        if (rset->disabled(*session)) {
+          disabled = true;
+          if (disabled_objects.find(app->UID()) != disabled_objects.end()) {
+            reason = "directly";
+          }
+          else {
+            reason = "due to state of related objects";
           }
         }
+        std::cout << " contains: {";
+        std::string seperator = "";
+        for (auto mod : rset->get_contains()) {
+          std::cout << seperator << mod->UID();
+          if (mod->disabled(*session)) {
+            std::cout << "<disabled ";
+            if (disabled_objects.find(mod->UID()) == disabled_objects.end()) {
+              std::cout << "in";
+            }
+            std::cout << "directly>";
+          }
+          seperator = ", ";
+        }
+        std::cout << "}";
       }
+    }
+    if (disabled) {
+      std::cout << " <disabled "<< reason << ">";
     }
     auto daqApp = app->cast<confmodel::DaqApplication>();
     if (daqApp) {
@@ -49,6 +73,7 @@ void process_segment(const confmodel::Session* session,
         std::cout << " " << mod->UID();
       }
     }
+
     std::cout << std::endl;
   }
 }
@@ -70,5 +95,11 @@ int main(int argc, char* argv[]) {
     return -1;
   }
 
-  process_segment (session, session->get_segment());
+  std::set<std::string> disabled_objects;
+  for (auto object : session->get_disabled()) {
+    TLOG_DEBUG(11) << object->UID() << " is in disabled list of Session";
+    disabled_objects.insert(object->UID());
+  }
+
+  process_segment (session, session->get_segment(), disabled_objects, "");
 }
