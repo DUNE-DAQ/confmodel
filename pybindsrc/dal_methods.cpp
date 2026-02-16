81bbf04e9783bb1961f23c6f61ef9464d370c380
Gordon Crone <g.crone@ucl.ac.uk>
2026-02-16 17:05:10 +0000

    Allow defaulting to session's root segment in segment_get_managed_object_tags python binding

--- pybindsrc/dal_methods.cpp
+++ pybindsrc/dal_methods.cpp
@@ -39,9 +39,12 @@ namespace dunedaq::confmodel::python {
 
   std::set<std::string>
   segment_get_managed_object_tags(Configuration& db,
-                                  const std::string& segment_name,
-                                  const std::string& session_name) {
+                                  const std::string& session_name,
+                                  std::string segment_name="") {
     auto session=db.get<Session>(session_name);
+    if (segment_name == "") {
+      segment_name = session->get_segment()->UID();
+    }
     auto segment=db.get<Segment>(segment_name);
     return segment->managed_object_tags(session);
   }
@@ -185,7 +188,9 @@ register_dal_methods(py::module& m)
     .def_readonly("class_name", &ObjectLocator::class_name)
     ;
 
-  m.def("segment_get_managed_object_tags", &segment_get_managed_object_tags, "Get list of ALL enabled ManagedObject tags in the requested segment");
+  m.def("segment_get_managed_object_tags", &segment_get_managed_object_tags,
+        py::arg("db"), py::arg("session_name"), py::arg("segment_name")="",
+        "Get list of ALL enabled ManagedObject tags in the requested segment");
   m.def("session_get_all_applications", &session_get_all_applications, "Get list of ALL applications (regardless of enabled/disabled state) in the requested session");
   m.def("session_get_enabled_applications", &session_get_enabled_applications, "Get list of enabled applications in the requested session");
 
